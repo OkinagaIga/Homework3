052dc5d83ba6d18f2de3c7ada90392384db5b534
OkinagaIga <okinagaiga@gmail.com>
2024-07-10 23:12:37 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,16 @@
+name = 'Daria'
+print(name)
+age = 38
+print(age)
+age = 38 + 2
+print(age)
+is_student = True
+print(is_student)
+name = 'Daria'
+print('Name:', name)
+age = 38
+print('Age:', age)
+age = 38 + 2
+print('Age:', age)
+is_student = True
+print('Is student:', is_student)
