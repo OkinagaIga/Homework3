772beece789d84d5c307c0d34e4b8346c7f37e4a
OkinagaIga <okinagaiga@gmail.com>
2024-07-11 13:23:30 +0300

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -2,7 +2,7 @@ name = 'Daria'
 print(name)
 age = 38
 print(age)
-age = 38 + 2
+age = age + 2
 print(age)
 is_student = True
 print(is_student)
@@ -10,7 +10,7 @@ name = 'Daria'
 print('Name:', name)
 age = 38
 print('Age:', age)
-age = 38 + 2
+age = age + 2
 print('Age:', age)
 is_student = True
 print('Is student:', is_student)
